80eaf58ad1cf7e25583a70872a49816675523a62
Christopher Peret <cperet89@gmail.com>
2023-03-25 00:34:09 -0700

    variables.js mod

---
+++ assets/js/variables.js
@@ -0,0 +1,10 @@
+// ***************
+// let variables
+// ***************
+let shuffledQuestions, currentQuestionIndex
+var timeLeft = 100;
+var playerScore = 0;
+let gameON = false;
+let highScores = [];
+
+
